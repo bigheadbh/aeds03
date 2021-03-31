b646ac8122341679bf3e49036ddb4e1cc814d386
bigheadbh <felipebh@gmail.com>
2021-03-30 22:06:22 -0300

    arvore binaria features

--- pesquisa/arvorebinaria.c
+++ pesquisa/arvorebinaria.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 typedef struct Registro {
-    long Chave;
+    long chave;
     int idade;
 } Registro;
 
@@ -12,16 +12,15 @@ typedef struct No{
     struct No* Dir;
 } No;
 
-
 void Pesquisa(Registro *x, No *p){
     if (p == NULL){
+        (*x).chave = -1;
         printf("Erro : Registro nao esta presente na arvore\n");
         return;
-    }
-    if (x->Chave < p->Reg.Chave) {
+    }else if (x->chave < p->Reg.chave) {
         Pesquisa(x, p->Esq);
         return;
-    } else if (x->Chave > p->Reg.Chave){ 
+    } else if (x->chave > p->Reg.chave){ 
         Pesquisa(x, p->Dir);
     } else {
         *x = p->Reg;
@@ -36,20 +35,25 @@ void Insere(Registro x, No **p) {
         (*p)->Dir = NULL;
         return;
     }
-    if (x.Chave < (*p)->Reg.Chave){
+    
+    if (x.chave < (*p)->Reg.chave){
+#ifdef DEBUG
+        printf("(ESQ) chave %ld - valor do no %ld\n", x.chave, (*p)->Reg.chave);
+#endif // DEBUG
         Insere(x, &(*p)->Esq);
         return;
-    } else if (x.Chave > (*p)->Reg.Chave){
+    } else if (x.chave > (*p)->Reg.chave){
+#ifdef DEBUG
+        printf("(DIR) chave %ld - valor do no %ld\n", x.chave, (*p)->Reg.chave);
+#endif // DEBUG
         Insere(x, &(*p)->Dir);
     } else {
         printf("Erro : Registro ja existe na arvore\n");
     }
 }
 
-void Antecessor(No *q, No **r)
-{
-    if ((*r)->Dir != NULL)
-    {
+void Antecessor(No *q, No **r){
+    if ((*r)->Dir != NULL){
         Antecessor(q, &(*r)->Dir);
         return;
     }
@@ -59,68 +63,93 @@ void Antecessor(No *q, No **r)
     free(q);
 }
 
-void Retira(Registro x, No **p)
-{
+void Retira(Registro x, No **p){
     No* Aux;
-    if (*p == NULL)
-    {
+    if (*p == NULL){
         printf("Erro : Registro nao esta na arvore\n");
         return;
     }
-    if (x.Chave < (*p)->Reg.Chave)
-    {
+    if (x.chave < (*p)->Reg.chave){
         Retira(x, &(*p)->Esq);
         return;
     }
-    if (x.Chave > (*p)->Reg.Chave)
-    {
+    if (x.chave > (*p)->Reg.chave){
         Retira(x, &(*p)->Dir);
         return;
     }
-    if ((*p)->Dir == NULL)
-    {
+
+    if ((*p)->Dir == NULL){ // nao tenho filho a direita e posso ter ou não a esquerda.
         Aux = *p;
         *p = (*p)->Esq;
         free(Aux);
         return;
     }
-    if ((*p)->Esq != NULL)
-    {
+    if ((*p)->Esq != NULL){ // o no tem dois filhos.
         Antecessor(*p, &(*p)->Esq);
         return;
     }
+    // nao tem filho a esquerda e tem a direita.
     Aux = *p;
     *p = (*p)->Dir;
     free(Aux);
 }
 
-void Central(No *p)
-{
+void Central(No *p){
     if (p == NULL)
         return;
     Central(p->Esq);
-    printf("%ld\n", p->Reg.Chave);
+    printf("%ld\t", p->Reg.chave);
     Central(p->Dir);
 }
 
-#define MAX 5
+void PreOrdem(No *p){
+    if (p == NULL)
+        return;
+    PreOrdem(p->Esq);
+    PreOrdem(p->Dir);
+    printf("%ld\t", p->Reg.chave);
+}
+
+void PosOrdem(No *p){
+    if (p == NULL)
+        return;
+    printf("%ld\t", p->Reg.chave);
+    PosOrdem(p->Esq);
+    PosOrdem(p->Dir);
+}
+
+#define MAX 10
 
 int main(int argc, char *argv[]){
     No *arvore = NULL;
    
     Registro x;
-
     /* Insere cada chave na arvore e testa sua integridade apos cada insercao */
     for (int i = 0; i < MAX; i++){
-        x.Chave = i+1;
+        x.chave = rand()%100;
         x.idade = 20+i;
         Insere(x, &arvore);
-        printf("Inseriu chave: %ld\n\n", x.Chave);
+#ifdef DEBUG
+        printf("Inseriu chave: %ld\n", x.chave);
+        getchar();
+#endif // DEBUG
     }
 
-    x.Chave = 5;
+    x.chave = 5;
     Pesquisa(&x, arvore);
-    printf("%d - a idade da chave 5\n", x.idade);
+    if(x.chave != -1)
+        printf("%d - a idade da chave 5\n", x.idade);
 
+    printf("[ ");
+    Central(arvore);
+    printf(" ]\n");
+
+    printf("[ ");
+    PosOrdem(arvore);
+    printf(" ]\n");
+
+    printf("[ ");
+    PreOrdem(arvore);
+    printf(" ]\n");
     return 0;
 }
