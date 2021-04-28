42d40ecf1e9dec096377d539240d6ebd104fbf6d
bigheadbh <felipebh@gmail.com>
2021-04-27 21:44:42 -0300

    argc & argv

---
+++ util/argc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char* argv[]){
+    for(int i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
+
+    if(strcmp(argv[1], "hash") == 0){
+        printf("Escolheu Hash\n");
+
+    }else if (strcmp(argv[1], "pseq") == 0){
+        printf("Escolheu Pesquisa Sequencial\n");
+
+    }else if (strcmp(argv[1], "arvore") == 0){
+        printf("Estrutura não implementada.\n");
+    }
+    return 0;
+}
