b179edff245190e8fbf91433d6714aa291746919
Felipe Domingos <felipebh@gmail.com>
2021-06-22 19:23:51 -0300

    Add pessoa.cpp

---
+++ poo/pessoa.cpp
@@ -0,0 +1,4 @@
+
+/* Crie uma classe para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os métodos públicos necessários para sets e gets e também um métodos para imprimir os dados de uma pessoa. */
+
+
